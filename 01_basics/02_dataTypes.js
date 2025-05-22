71ef87037b921f23078e6ad1baadd83bf322b050
Somya-keshri <keshrisomya9596@gmail.com>
2025-05-22 19:35:14 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3); we are using nodejs, not browser
+console.log(5+
+    6);   // code readability should be high
+console.log("Hello")
+
+let name = "Harry"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// Number => 2 to power 53
+// bigInt => 
+// String => ""
+// Boolean => true/false
+// Null => standalone value
+// undefined
+// Symbol => unique
+
+// Object
+
+console.log(typeof age);  // number
+console.log(typeof name);  // string
+console.log(typeof isLoggedIn);  //boolean
+console.log(typeof null);   // object
+console.log(typeof undefined);  //undefined
