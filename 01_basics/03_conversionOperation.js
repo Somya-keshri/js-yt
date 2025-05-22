1ca7bd568dd08bed10d691276457f2bf087a272c
Somya-keshri <keshrisomya9596@gmail.com>
2025-05-22 20:30:47 +0000

    ConversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33Abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNum = Number(score)
+
+// console.log(typeof valueInNum)
+// console.log(valueInNum)
+
+// "33" => 33
+// "53abc" => NaN
+// true => 1; false => 0
+// null => 0
+// umdefined => NaN
+
+let isLoggedIn = false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+/// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "ghhc" => true
+
+let someNum = 57
+
+let stringNumber = String(someNum)
+console.log(stringNumber);  // 57
+console.log(typeof stringNumber);  // string
